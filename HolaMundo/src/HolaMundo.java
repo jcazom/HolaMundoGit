1fd040ffa837cf71587949b8abeef35c0189b7bd
debian <debian@Debian>
2022-02-21 20:00:16 +0100

    3 Cambio Eclipse

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -3,8 +3,12 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
+		System.out.println("1 cambio - Consola");
 		System.out.println("1 cambio - Eclipse");
+		System.out.println("2 cambio - Consola");
 		System.out.println("2 cambio - Eclipse");
+		System.out.println("3 cambio - Consola");
+		System.out.println("3 cambio - Eclipse");
 
 	}
 
